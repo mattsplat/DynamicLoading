ad4f5b7fb93cc508e5ba4a151ac5567e710739fc
matt <matthewjohncoleman@gmail.com>
2020-09-20 18:26:54 -0500

    initial push

---
+++ src/DynamicLoad.php
@@ -0,0 +1,83 @@
+<?php
+
+
+namespace MattSplat\DynamicLoading;
+
+
+use Closure;
+use Illuminate\Database\Eloquent\Collection;
+
+class DynamicLoad
+{
+    public function load(Collection $models, string $relation_name, Closure $subQuery, $relation_key = null, $model_key = null) : Collection
+    {
+
+        $queries = new Collection();
+        if(!$model_key) {
+            $model_key = $models->first()->getKeyName();
+        }
+        if(!$relation_key) {
+            $class = class_basename($models->first());
+            $class = strtolower(preg_replace('/(.)(?=[A-Z])/u', '$1'.'_', $class));
+            $relation_key = $class . '_' . $model_key;
+        }
+
+        foreach ($models as $model) {
+
+            $query = call_user_func($subQuery, $model);
+
+            if(!$this->checkIfRelationKeyExistsInQuery($query, $relation_key)) {
+                $query->addSelect($model->{$model_key} . ' as ' . $relation_key);
+            }
+
+            $queries->push(
+                $query
+            );
+        }
+
+
+        /// combine union queries together
+        $q = $queries->first();
+        for ($x = 1; $x < count($queries); $x++) {
+            $q = $q->union($queries[$x]);
+        }
+
+        try {
+            $relations = $q->get()->groupBy($relation_key);
+        } catch (\Exception $e) {
+            return $models;
+        }
+
+
+        foreach ($models as $model) {
+            if (isset($relations[$model->{$model_key}])) {
+                $model->setRelation($relation_name, $relations[$model->{$model_key}]);
+            }
+        }
+
+        return $models;
+    }
+
+    /**
+     * @param $query
+     * @param string|null $relation_key
+     */
+    public function checkIfRelationKeyExistsInQuery($query, ?string $relation_key): bool
+    {
+        $columns = $query->getQuery()->columns;
+        if(empty($columns)) {
+            $query->select('*');
+            return false;
+        }
+
+        $has_relation_key = false;
+        foreach ($columns as $column) {
+            foreach (explode(' ', $column) as $part) {
+                if ($part === $relation_key) $has_relation_key = true;
+            }
+
+        }
+
+        return $has_relation_key;
+    }
+}
---
+++ src/DynamicLoadingServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace MattSplat\DynamicLoading;
+
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+
+class DynamicLoadingServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        Collection::macro('dynamicLoad', function ($relation_name, $subQuery, $model_id = null, $relation_id = null) {
+
+            $newCollection = collect();
+
+            if ($this->isNotEmpty()) {
+                $loader =  new DynamicLoad();
+                $newCollection = $loader->load($this, $relation_name, $subQuery, $model_id, $relation_id);
+            }
+
+            return $newCollection;
+
+        });
+    }
+
+    public function boot()
+    {
+
+    }
+}
